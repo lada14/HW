aa74a10da12ccbb5220c2e05c89c14018f8e7c01
229195-18 <229195-18>
2021-04-14 11:49:17 +0300

    1

---
+++ 31/ConsoleApplication31/ConsoleApplication31.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class School {
+public:
+	~School() {
+		cout << "School delete";
+	}
+	int klass;
+	int kabinet;
+	school(int k, int kad) {
+		конструктор
+	}
+
+};
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	vector<School*> school;
+	school.
+
+	School *uk = new School;
+
+	delete uk;
+}
